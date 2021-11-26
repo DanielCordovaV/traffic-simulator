585df7c5c5cd04df8c49b4404fcda39e6d82ba35
Jorge Lopez <jlopezftw81@gmail.com>
2021-11-25 19:54:18 -0800

    Cars and streets can now be instantiated

--- frontend/Assets/Scripts/GameManager.cs
+++ frontend/Assets/Scripts/GameManager.cs
@@ -1,15 +1,19 @@
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class GameManager : MonoBehaviour
 {
     [SerializeField] private GameObject requestManager;
     [SerializeField] private GameObject carPrefab;
+    [SerializeField] private GameObject streetPrefab;
+    [SerializeField] private NavMeshSurface mesh;
     
     private Requesting requestingScript;
     private Data positions;
 
     private List<GameObject> cars = new List<GameObject>();
+    private List<GameObject> streets = new List<GameObject>();
 
     void Start()
     {
@@ -22,7 +26,17 @@ public class GameManager : MonoBehaviour
             {
                 GameObject newGO = Instantiate(carPrefab, new Vector3(0, 0, 0), Quaternion.identity);
                 cars.Add(newGO);
+                newGO.transform.parent = GameObject.Find("Cars").transform;
             }
+            for (int i = 0; i < requestingScript.positionsQueue.Count; i++)
+            {
+                GameObject newGO = Instantiate(streetPrefab, new Vector3(0, 0, 0), Quaternion.identity);
+                streets.Add(newGO);
+                newGO.transform.parent = GameObject.Find("Road").transform;
+            }
+
+            mesh = streets[0].GetComponent<NavMeshSurface>();
+            mesh.BuildNavMesh();
         }
     }
 
