8bbd1e6fa997193e58979b2f5f57136dc14aa325
SebastianMarAlv <sebastianmarquez0@gmail.com>
2021-11-26 01:40:59 -0600

    TrafficLightAgent fully functioning

--- src/TrafficLightAgent.py
+++ src/TrafficLightAgent.py
@@ -15,14 +15,13 @@ class TrafficLightAgent(ap.Agent):
     there_is_main = False
 
     def setup(self, direction: tuple[int, int]) -> None:
-        self.condition = 2
         self.__current_light = LightColor.YELLOW
         self.__timer = 0
         self.is_main = False
         self.got_main = False
         main_traffic_light: TrafficLightAgent
         self.direction = direction
-        if direction == Direction.DOWN or direction == Direction.Up:
+        if direction == Direction.DOWN.value or direction == Direction.UP.value:
             self.orientation = Orientation.VERTICAL
         else:
             self.orientation = Orientation.HORIZONTAL
@@ -34,7 +33,7 @@ class TrafficLightAgent(ap.Agent):
         self.__timer = 0
 
     def set_as_main_light(self, agent: ap.Agent) -> None:
-        self.__current_light == LightColor.GREEN
+        self.__current_light = LightColor.GREEN
         self.main_traffic_light = agent
         self.got_main = True
 
@@ -51,18 +50,18 @@ class TrafficLightAgent(ap.Agent):
                 break
 
     def check_for_incoming_cars(self) -> None:
-        neighbours: ap.AgentIter = self.model.city.neighbors(self, distance=5)
+        neighbours: ap.AgentIter = self.model.grid.neighbors(self, distance=6)
         neighbour: ap.Agent
         for neighbour in neighbours:
-            if neighbour.type is CarAgent:
+            if neighbour.type == 'CarAgent':
                 if not TrafficLightAgent.there_is_main:
                     self.make_main()
                 else:
                     self.get_main()
 
     def operate_as_main(self):
-        if self.timer < TrafficLightAgent.MAX_TIMER:
-            self.timer += 1
+        if self.__timer < TrafficLightAgent.MAX_TIMER:
+            self.__timer += 1
         else:
             if self.__current_light is LightColor.GREEN:
                 self.__current_light = LightColor.RED
@@ -88,7 +87,5 @@ class TrafficLightAgent(ap.Agent):
                     self.operate_as_slave()
             else:
                 self.get_main()
-                self.check_status()
         else:
             self.check_for_incoming_cars()
-            self.check_status()
